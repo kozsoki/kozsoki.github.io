8fe56c904b78ddb241045a84e61a0a13d9f50034
Korosi Zsombor <korosizsombor@gmail.com>
2017-11-14 15:57:35 +0100

    First commit

---
+++ commits.sql
@@ -0,0 +1,6 @@
+git remote add origin https://github.com/kozsoki/wiki.github.io.git
+git add .
+git commit -m "First commit"
+git push origin master
+git branch wiki-github
+git push origin wiki-github
